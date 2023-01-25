7307bda9e7b7c9131227befcf31fab434f0daa5e
Dorin-Ioan Ciora <dorin.ciora@fortech.ro>
2023-01-25 23:41:24 +0200

    adding modal service

---
+++ src/app/shared/modal/modal.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { ModalService } from 'src/app/services/modal.service';
+
+@Component({
+  selector: 'app-modal',
+  templateUrl: './modal.component.html',
+  styleUrls: ['./modal.component.less']
+})
+export class ModalComponent implements OnInit {
+
+  constructor(public modalService: ModalService) {
+  }
+
+  ngOnInit(): void {
+  }
+
+  public closeModal() {
+    this.modalService.toggleModal();
+  }
+
+}
